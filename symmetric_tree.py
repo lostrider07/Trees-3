5084e7aef93b788ddb9a251cb43bb63554056e6b
Saiteja Garlapati <86909787+lostrider07@users.noreply.github.com>
2022-08-26 15:36:45 -0700

    Update symmetric_tree.py

--- symmetric_tree.py
+++ symmetric_tree.py
@@ -1,3 +1,31 @@
+#BFS Approach
+#Time Complexity : O(n) 
+#Space Complexity :O(n)  
+#Did this code successfully run on Leetcode : Yes
+#Any problem you faced while coding this : No
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
+        q = deque()
+        q.append(root.left)
+        q.append(root.right)
+        
+        while q:
+            left = q.popleft()
+            right = q.popleft()
+            if left == None and right == None:
+                continue
+            if (left == None) or (right == None) or (left.val != right.val):
+                return False
+            q.append(left.left)
+            q.append(right.right)
+            q.append(left.right)
+            q.append(right.left)
+        return True
+
+
 #DFS Approach
 #Time Complexity : O(n) 
 #Space Complexity :O(h) 
